79555e31c61998dca1bbb37466f803af75430bba
skthdus <ssoy617@gmail.com>
2022-09-30 17:33:14 +0900

    9/30 실습

--- project03/src/App.js
+++ project03/src/App.js
@@ -5,6 +5,7 @@ import Ex02 from './Example/Ex02';
 import Ex03 from './Example/Ex03';
 import Ex04 from './Example/Ex04';
 import Ex05 from './Example/Ex05';
+import Ex06 from './Example/Ex06';
 
 
 function App() {
@@ -25,9 +26,10 @@ function App() {
     {/* <Ex04/> */}
 
     {/* ex05 : Map 함수를 활용한 콜렉션 실습! */}
-    <Ex05/>
+    {/* <Ex05/> */}
 
-
+    {/* ex06 : 미니실습! - 댓글달기 */}
+    <Ex06/>
     </div>
   );
 }
